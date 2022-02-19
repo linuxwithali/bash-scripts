e748395f99f1ce1bec71ea2e3b1064588fd93a01
Ali <38080361+linuxwithali@users.noreply.github.com>
2022-02-19 20:08:27 +0530

    Image Resize Script

---
+++ bulkImageResize.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Goal : batch image resizer
+# Source: https://guides.wp-bullet.com
+
+# absolute path to image folder
+FOLDER="/home/titus/website/content/wp-content"
+
+# max width
+WIDTH=1280
+
+# max height
+HEIGHT=720
+
+#resize png or jpg to either height or width, keeps proportions using imagemagick
+find ${FOLDER} -iname '*.jpg' -o -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize png to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.png' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+#resize jpg only to either height or width, keeps proportions using imagemagick
+#find ${FOLDER} -iname '*.jpg' -exec convert \{} -verbose -resize $WIDTHx$HEIGHT\> \{} \;
+
+# alternative
+#mogrify -path ${FOLDER} -resize ${WIDTH}x${HEIGHT}% *.png -verbose
