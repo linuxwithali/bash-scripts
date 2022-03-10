37badd9a1684e5f8d93643982b9b2a4a62101b14
Ali <38080361+linuxwithali@users.noreply.github.com>
2022-03-10 18:22:37 +0530

    Created obsSetup.sh

---
+++ obsSetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install ffmpeg -y
+sudo apt install v4l2loopback-dkms -y
+ sudo add-apt-repository ppa:obsproject/obs-studio
+ sudo apt update -y
+ sudo apt install obs-studio -y
+
