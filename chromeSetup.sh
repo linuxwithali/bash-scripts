e91f655142533b7dcff5b247475c09416ac87c2a
Ali <38080361+linuxwithali@users.noreply.github.com>
2022-03-10 18:18:44 +0530

    Create chromeSetup.sh

---
+++ chromeSetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#GOAL: Install Chrome Browser with APT Repo.
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt update -y 
+sudo apt install google-chrome-stable -y
